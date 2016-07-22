b5210f486c1a66e263630a54a4c6add57c1fa39a
Jaden Yuros <jaden.yuros@outlook.com>
2016-07-22 12:16:21 -0700

    Client Bugs

--- chatclient.c
+++ chatclient.c
@@ -25,45 +25,62 @@ void error(const char *msg)
     exit(1);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
-    char buffer[256];
-    if (argc < 3) {
-        fprintf(stderr,"usage %s hostname port\n", argv[0]);
-        exit(0);
+    // check that we have the right amount of arguments
+    char buffer[BUFFER_SIZE];
+    if (argc > 3) {
+        printf("usage error, try: %s hostname port\n", argv[0]);
     }
-    portno = atoi(argv[2]);
+
+    //create new socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-        error("ERROR opening socket");
+    if (sockfd < 0) {
+        fprintf(stderr,"ERROR: Could not open socket\n");
+        exit(1);
+    }
+
+    // setup server
     server = gethostbyname(argv[1]);
     if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
+        error("ERROR: no such host\n");
     }
+
+    // clear memory and get port number
     bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[2]);
+
+    // build the struct
     serv_addr.sin_family = AF_INET;
     bcopy((char *)server->h_addr,
           (char *)&serv_addr.sin_addr.s_addr,
           server->h_length);
     serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
-        error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0)
-        error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0)
-        error("ERROR reading from socket");
-    printf("%s\n",buffer);
+
+    // Attempt to bind socket with port
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){
+        error("ERROR: on binding\n");
+    }
+
+    // get message from the user
+    printf("Enter the message:  ");
+    bzero(buffer, BUFFER_SIZE);
+    fgets(buffer, BUFFER_SIZE-1, stdin);
+    n = write(sockfd, buffer, strlen(buffer));
+    if (n < 0) {
+        error("ERROR: writing to the socket\n");
+    }
+    bzero(buffer, BUFFER_SIZE);
+    n = read(sockfd, buffer, BUFFER_SIZE-1);
+    if (n < 0) {
+        error("ERROR: reading from the socket\n");
+    }
+    printf("MSG:\t%s\n", buffer);
     close(sockfd);
     return 0;
+
 }
