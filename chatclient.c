92ca2c6bfae04686b7cac4e7631df1db747e1d42
Jaden Yuros <jaden.yuros@outlook.com>
2016-07-24 18:51:34 -0700

    Client is working

--- chatclient.c
+++ chatclient.c
@@ -25,8 +25,80 @@ void error(const char *msg)
     exit(1);
 }
 
+// sets up the connection from this client to server
+int connectToServer(struct hostent *server, int portno){
+
+    //create new socket
+    int sockfd;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0){
+        error("ERROR opening socket");
+        return -1;
+    }
+
+    struct sockaddr_in serv_addr;
+
+    // clear memory of the struct
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+
+    // build the struct
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,
+          (char *)&serv_addr.sin_addr.s_addr,
+          server->h_length);
+    serv_addr.sin_port = htons(portno);
+
+    // attempt to connect to server
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) {
+        error("ERROR connecting");
+        return -1;
+    }
+    return sockfd;
+}
+
+
+
+
+// get users chat handle. Can be up to 10 chars long + \n + \0. with 500 char limit on message
+// we only need a 512 byte block of memory to send entire handle and message
+void createHandle(char handle[12]) {
+
+    memset(handle,'\0', 12);
+    printf("Please enter your chat handle(up to 10 characters):  ");
+    if ((fgets(handle, 11, stdin) == NULL)) {
+        fprintf(stderr, "Error reading handle.\n");
+        exit(1);
+    }
+    // trims off the newline char from input
+    handle[strcspn(handle, "\n")] = 0;
+
+    // clears the input stream if there are extra characters entered
+    if(strlen(handle) > 9) {
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF );
+    }
+    // adds the '>' and the ' ' chars to handle
+    handle[strlen(handle)] = '>';
+    handle[strlen(handle)] = ' ';
+}
+
+void sendInitialMessage(int socketfd, char handle[12], char* port) {
+    int n;
+    char message[BUFFER_SIZE];
+    memset(message,'\0', BUFFER_SIZE);
+    strcpy(message, handle);
+    strcat(message, port);
+    n = write(socketfd, &message, strlen(message));
+    if (n < 0) {
+        error("ERROR sending message\n");
+    }
+
+}
+
 int sendMessage (int socketfd, char handle[12]) {
     int n;
+    //setup and clear message space
     char message[BUFFER_SIZE];
     memset(message,'\0', BUFFER_SIZE);
     strcpy(message, handle);
@@ -39,7 +111,7 @@ int sendMessage (int socketfd, char handle[12]) {
     printf(handle);
     if ((fgets(messageBuffer, MESSAGE_SIZE-1, stdin) == NULL)) {
         fprintf(stderr, "Error reading message.\n");
-        exit(1);
+        return -1;
     }
     // trims off the newline char from input
     messageBuffer[strcspn(messageBuffer, "\n")] = 0;
@@ -55,30 +127,49 @@ int sendMessage (int socketfd, char handle[12]) {
         n = write(socketfd, &messageBuffer, strlen(messageBuffer));
         if (n < 0) {
             error("ERROR sending message\n");
-            exit(1);
+            return -1;
         }
         printf("Chat session ended\n");
         close(socketfd);
-        exit(0);
+        return -2;
 
     }
-
+        // if quit message is not found, concat the handle and the message and send to server
     else {
         strcat(message, messageBuffer);
         n = write(socketfd, &message, strlen(message));
         if (n < 0) {
             error("ERROR sending message\n");
-            exit(1);
+            return -1;
         }
     }
 
     return 0;
 }
 
+// read message from socketfd
+int readMessage(int socketfd) {
+    int n;
+    char message[MESSAGE_SIZE];
+    memset(message,'\0', MESSAGE_SIZE);
+
+    n = read(socketfd, message, BUFFER_SIZE-1);
+    if (n < 0) {
+        error("ERROR reading from the socket\n");
+    }
+    printf("%s\n", message);
+
+    if(strstr(message, "\\quit") != NULL) {
+        printf("Server has closed the connection\n");
+        return -1;
+    }
+    return 0;
+}
+
 int main(int argc, char *argv[]) {
 
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
+    int n;
+    int portno;
     struct hostent *server;
 
     // check that we have the right amount of arguments
@@ -88,81 +179,35 @@ int main(int argc, char *argv[]) {
         exit(0);
     }
 
-    // get port number
-    portno = atoi(argv[2]);
-
-    //create new socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-        error("ERROR opening socket");
-
-    // setup server
+    // get server name
     server = gethostbyname(argv[1]);
     if (server == NULL) {
         fprintf(stderr, "ERROR, no such host\n");
         exit(0);
     }
 
-    // clear memory of the struct
-    bzero((char *) &serv_addr, sizeof(serv_addr));
+    // get port number
+    portno = atoi(argv[2]);
 
-    // build the struct
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr,
-          (char *)&serv_addr.sin_addr.s_addr,
-          server->h_length);
-    serv_addr.sin_port = htons(portno);
+    // attempt to connect client to server
+    int sockfd = connectToServer(server, portno);
 
-    // attempt to connect to server
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
-        error("ERROR connecting");
-
-
-
-
-
-    // get users chat handle. Can be up to 10 chars long + \n + \0. with 500 char limit on message
-    // we only need a 512 byte block of memory to send entire handle and message
+    // create the users chat handle
     char handle[12];
-    memset(handle,'\0', 12);
-    printf("Please enter your chat handle(up to 10 characters):  ");
-    if ((fgets(handle, 11, stdin) == NULL)) {
-        fprintf(stderr, "Error reading handle.\n");
-        exit(1);
-    }
-    // trims off the newline char from input
-    handle[strcspn(handle, "\n")] = 0;
+    createHandle(handle);
 
-    // clears the input stream if there are extra characters entered
-    if(strlen(handle) > 9) {
-        int c;
-        while ((c = getchar()) != '\n' && c != EOF );
-    }
-    // adds the '>' char to handle
-    handle[strlen(handle)] = '>';
+    // send the initial communication message with handle and port
+    sendInitialMessage(sockfd, handle, argv[2]);
 
-    // send sever the port number
-    bzero(buffer, BUFFER_SIZE);
-    strcpy(buffer, handle);
-    strcat(buffer, argv[2]);
-    n = write(sockfd, buffer, strlen(buffer));
-    if (n < 0) {
-        error("ERROR writing to the socket\n");
-        close(sockfd);
-        exit(1);
+    int chat;
+    chat = 0;
+
+    // send and receive messages until 'quit' is entered
+    while(chat == 0){
+        chat = readMessage(sockfd);
+        chat = sendMessage(sockfd, handle);
     }
 
-
-
-
-
-    bzero(buffer, BUFFER_SIZE);
-    n = read(sockfd, buffer, BUFFER_SIZE-1);
-    if (n < 0) {
-        error("ERROR reading from the socket\n");
-    }
-    printf("MSG:\t%s\n", buffer);
     close(sockfd);
     return 0;
-
 }
--- chatserve.py
+++ chatserve.py
@@ -2,46 +2,69 @@
 import socket
 import sys
 
-# Create a TCP/IP Socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Allow sockets to be reused if not closed properly
-# sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-print('Socket created')
+def setupConnection(port):
+    # Create a TCP/IP Socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Allow sockets to be reused if not closed properly
+    # sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    print('Socket created')
 
-# Bind the socket to port
-HOST = socket.gethostname()
-PORT = 25552
-print('HOST: ', HOST)
-print('PORT: ', PORT)
-try:
-    sock.bind((HOST, PORT))
-except socket.error as msg:
-    print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-    sys.exit()
-print('Socket bind complete')
-
-# Listen for up to 5 incoming connections
-sock.listen(5)
-
-while True:
-    # Wait for a connection
-    print('Socket now listening')
-    connection, client_address = sock.accept()
-
+    # Bind the socket to port
+    HOST = socket.gethostname()
+    PORT = int(port)
+    print('HOST: ', HOST)
+    print('PORT: ', PORT)
     try:
-        print('connection from', client_address)
+        sock.bind((HOST, PORT))
+    except socket.error as msg:
+        print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+        sys.exit()
+    print('Socket bind complete')
 
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(500)
-            print('received "%s"' % data)
-            if data:
-                print('sending data back to the client')
-                connection.sendall(data)
-            else:
-                print('no more data from', client_address)
-                break
+    # Listen for up to 5 incoming connections
+    sock.listen(5)
+    return sock
 
-    finally:
-        # Clean up the connection
-        connection.close()
+
+
+
+def chat(connection):
+    exitMessage = "\quit"
+    while True:
+        clientMessage = ''
+        clientMessage = connection.recv(500)
+        if not clientMessage:
+            print(sys.stderr, 'ERROR receiving client chat data...')
+            sys.exit(1)
+        print(clientMessage)
+        if clientMessage.find(exitMessage) != -1:
+            print('Client closed the connection')
+            connection.close()
+            break
+        # server handle is 8 chars long
+        prompt = 'SERVER> '
+        message = raw_input(prompt)
+        serverChat = prompt + message
+        # only get the first 500 chars
+        serverMessage = serverChat[:500]
+        connection.send(serverMessage)
+        if serverMessage.find(exitMessage) != -1:
+            print('Server closed the connection')
+            connection.close()
+            break
+
+def main(argv):
+    sock = setupConnection(argv[1])
+    while True:
+        # Wait for a connection
+        print('Socket now listening')
+        connection, client_address = sock.accept()
+
+        try:
+            print('connection from', client_address)
+            chat(connection)
+        finally:
+            # Clean up the connection
+            connection.close()
+
+main(sys.argv)
